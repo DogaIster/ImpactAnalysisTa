32aae1cb403ae07ac3eb46968ca0ee6ef9ec13c7
Doga Ister <doga.ister@mail.utoronto.ca>
2016-10-17 14:13:45 -0400

    Second version of this file

--- ImpactAnalysis.java
+++ ImpactAnalysis.java
@@ -3,25 +3,41 @@ package com.atlassian.jira.plugins.panel.issue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel2;
+import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueAction;
+import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanelModuleDescriptor;
 import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
 import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel2;
-import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.plugin.issuetabpanel.GetActionsRequest;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.jira.user.ApplicationUser;
 import java.util.Collections;
 import java.util.List;
+import java.util.Date;
+import java.util.Map;
+import java.util.ArrayList;
 
-public abstract class ImpactAnalysis extends AbstractIssueTabPanel2 implements IssueTabPanel2
-{
-    private static final Logger log = LoggerFactory.getLogger(ImpactAnalysis.class);
+public class ImpactAnalysis extends AbstractIssueAction{ 
+	private final JiraAuthenticationContext authenticationContext; 
+	public ImpactAnalysis(IssueTabPanelModuleDescriptor descriptor, JiraAuthenticationContext authenticationContext) { 
+		super(descriptor); this.authenticationContext = authenticationContext; } 
+	@Override public Date getTimePerformed() { 
+		return new Date(); } 
+	@Override protected void populateVelocityParams(Map params) { 
+		params.put("user", this.authenticationContext.getUser().getName()); } 
+
 
     public List getActions(Issue issue, User remoteUser) {
-        return Collections.singletonList(new GenericMessageAction("Welcome!"));
-    }
+		List<ImpactAnalysis> panelActions = new ArrayList<ImpactAnalysis>();
+		panelActions.add(new ImpactAnalysis(descriptor, authenticationContext));
+		return panelActions;
+	}
 
     public boolean showPanel(Issue issue, User remoteUser)
     {
         return true;
     }
 }
+
